ff80103123882424572e4241dd68ab8274d409f7
Anatoliy Zolotukhin <zolotukhinwebpr@gmail.com>
2023-11-21 02:22:02 +0700

    fix: added logs

--- src/pages/api/identity/email/signup.ts
+++ src/pages/api/identity/email/signup.ts
@@ -7,8 +7,12 @@ export default async function handler(
 ) {
   try {
     const response = await ServiceIdentity.emailSignUp(req.body, {});
+    // eslint-disable-next-line no-console
+    console.log({ response });
     return res.send({ response });
-  } catch (error) {
-    return res.end(error);
+  } catch (error: any) {
+    // eslint-disable-next-line no-console
+    console.log({ error });
+    return res.status(error.response.status).end(error.response.data);
   }
 }
