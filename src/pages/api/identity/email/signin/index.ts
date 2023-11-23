8f0efd70c6177274677678756662ee661d1d6a8b
Anatoliy Zolotukhin <zolotukhinwebpr@gmail.com>
2023-11-24 00:20:53 +0700

    test

--- src/pages/api/identity/email/signin/index.ts
+++ src/pages/api/identity/email/signin/index.ts
@@ -11,6 +11,7 @@ export default async function handler(
     return res.send(response.data);
   } catch (error) {
     if (isAxiosError(error)) {
+      // eslint-disable-next-line no-console
       console.log({ error });
       // @ts-ignore
       return res.status(500).send();
