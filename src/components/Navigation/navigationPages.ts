59650fb6c1f996ae78f3f30b54107430b2b6bcee
Anatoliy Zolotukhin <zolotukhinwebpr@gmail.com>
2024-01-27 20:32:23 +0700

    feat: added faq page to nav

--- src/components/Navigation/navigationPages.ts
+++ src/components/Navigation/navigationPages.ts
@@ -8,18 +8,16 @@ const navigationPages = [
     //   href: "/demos/ecommerce/listing",
     //   id: "demo__ecommerece-listing--search",
     // },
-    // {
-    //   title: "Promotions",
-    //   href: "/demos/ecommerce/promotions",
-    //   id: "demo__ecommerece-listing--promotions",
-    // },
-    // ],
   },
-
   {
     title: "Тарифы",
     id: "tariff",
     href: "/tariffs",
+  },
+  {
+    title: "FAQ",
+    id: "faq",
+    href: "/faq",
   },
   {
     title: "Компания",
